6c19fd54a4ec18fca8a2967db9f0b0b0041bd514
Lordalt <104421223+Lordalt@users.noreply.github.com>
2023-06-11 18:15:09 +0200

    Add files via upload

---
+++ platform/vendor/mediatek/proprietary/external/strongswan/config.h
@@ -0,0 +1,176 @@
+
+
+
+
+
+
+
+#define CONFIG_H_INCLUDED 
+
+
+
+#define HAVE_ALLOCA 1
+
+#define HAVE_ALLOCA_H 1
+
+#define HAVE_BACKTRACE 1
+
+
+#define HAVE_CLOCK_GETTIME 1
+
+
+#define HAVE_CONDATTR_CLOCK_MONOTONIC 
+
+#define HAVE_DECL_STRERROR_R 1
+
+#define HAVE_DLADDR 
+
+#define HAVE_DLFCN_H 1
+
+#define HAVE_FMEMOPEN 1
+
+
+#define HAVE_GCC_ATOMIC_OPERATIONS 
+
+
+#define HAVE_GETGRNAM_R 1
+
+#define HAVE_GETPASS 1
+
+#define HAVE_GETPWNAM_R 1
+
+#define HAVE_GETPWUID_R 1
+
+#define HAVE_GETTID 
+
+#define HAVE_GLOB_H 1
+
+#define HAVE_IN6ADDR_ANY 
+
+#define HAVE_IN6_PKTINFO 
+
+#define HAVE_INTTYPES_H 1
+
+#define HAVE_IPSEC_DIR_FWD 
+
+#define HAVE_IPSEC_MODE_BEET 
+
+#define HAVE_LIBGMP 1
+
+
+
+#define HAVE_LINUX_UDP_H 1
+
+#define HAVE_MALLINFO 1
+
+#define HAVE_MEMORY_H 1
+
+#define HAVE_MEMRCHR 1
+
+#define HAVE_MMAP 1
+
+
+
+#define HAVE_NETINET_IP6_H 1
+
+
+
+
+#define HAVE_PRCTL 1
+
+#define HAVE_PRINTF_FUNCTION 
+
+
+#define HAVE_PTHREAD_CANCEL 1
+
+#define HAVE_PTHREAD_CONDATTR_INIT 1
+
+
+
+#define HAVE_PTHREAD_RWLOCK_INIT 1
+
+#define HAVE_PTHREAD_SPIN_INIT 1
+
+#define HAVE_QSORT_R 
+
+
+#define HAVE_QSORT_R_GNU 
+
+
+#define HAVE_RTA_TABLE 
+
+#define HAVE_SEM_TIMEDWAIT 1
+
+
+#define HAVE_STDBOOL_H 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_STRERROR_R 1
+
+#define HAVE_STRINGS_H 1
+
+#define HAVE_STRING_H 1
+
+#define HAVE_STRUCT_SADB_X_POLICY_SADB_X_POLICY_PRIORITY 1
+
+
+
+#define HAVE_SYS_GETTID 
+
+
+#define HAVE_SYS_STAT_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_UNISTD_H 1
+
+#define HAVE__BOOL 1
+
+
+
+#define LT_OBJDIR ".libs/"
+
+
+
+
+#define PACKAGE "strongswan"
+
+#define PACKAGE_BUGREPORT ""
+
+#define PACKAGE_NAME "strongSwan"
+
+#define PACKAGE_STRING "strongSwan 5.1.2"
+
+#define PACKAGE_TARNAME "strongswan"
+
+#define PACKAGE_URL ""
+
+#define PACKAGE_VERSION "5.1.2"
+
+
+#define STDC_HEADERS 1
+
+
+
+
+#define USE_IKEV1 
+
+#define USE_IKEV2 
+
+
+#define VERSION "5.1.2"
+
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+# endif
+#endif
+
+#define YYTEXT_POINTER 1
+
